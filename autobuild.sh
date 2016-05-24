3486c24b0899528011c7bb3b8071668c71456435
MartinMikita <martin.mikita@klokantech.com>
2016-05-24 15:31:36 +0200

    Changed autobuild to accept only tags without commits - new release.

--- autobuild.sh
+++ autobuild.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
-source activate
-production build app
-echo docker push klokantech/jekylledit:$(_version)
-docker push klokantech/jekylledit:$(_version)
-docker tag klokantech/jekylledit:$(_version) klokantech/jekylledit:latest
-docker push klokantech/jekylledit:latest
+if [ `git describe --tags --long | cut -d- -f2` -eq 0 ]; then
+    source activate
+    production build app
+    echo docker push klokantech/jekylledit:$(_version)
+    docker tag klokantech/jekylledit:$(_version) klokantech/jekylledit:latest
+    docker push klokantech/jekylledit:$(_version)
+    docker push klokantech/jekylledit:latest
+fi
