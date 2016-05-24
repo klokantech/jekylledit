37b463d350bed7607af0b7b259734825187931fd
MartinMikita <martin.mikita@klokantech.com>
2016-05-24 12:15:34 +0200

    Change _version to get full tag name. Added push this tag as latest.

--- autobuild.sh
+++ autobuild.sh
@@ -4,3 +4,5 @@ source activate
 production build app
 echo docker push klokantech/jekylledit:$(_version)
 docker push klokantech/jekylledit:$(_version)
+docker tag klokantech/jekylledit:$(_version) klokantech/jekylledit:latest
+docker push klokantech/jekylledit:latest
